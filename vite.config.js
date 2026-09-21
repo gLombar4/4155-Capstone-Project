c6aceffb66e2914354be5ac971e1123a031f5d2e
gLombar4 <lombard.garrett@gmail.com>
2026-09-21 14:58:37 -0400

    Vite Config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig, loadEnv } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '')
 
